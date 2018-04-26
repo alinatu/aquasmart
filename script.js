bd69178c7ba01d8b989f964f12f37f6fc3d4f99b
TanikaCharles <tanikacharles@gmail.com>
2018-04-26 12:35:55 -0700

    added water drop onClick

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 var name;
 var cityName;
+var waterLevel = "<img src='images/waterdrop.png' alt='water'>";
 function myFunction() {
     var name = document.getElementById("name").value;
     var cityName = document.getElementById("cityName").value;
     document.getElementById("userName").innerHTML = name;
     document.getElementById("userCity").innerHTML = cityName;
+  document.getElementById("waterLevel").innerHTML = waterLevel;
 }
