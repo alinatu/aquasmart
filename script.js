e436fedc5c4467166f245675bcad192d7d77311e
TanikaCharles <tanikacharles@gmail.com>
2018-04-26 16:09:55 -0700

    form and display editing

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var waterLevel = "<img src='images/waterdrop.png' alt='water'>";
 function myFunction() {
     var name = document.getElementById("name").value;
     var cityName = document.getElementById("cityName").value;
-    document.getElementById("userName").innerHTML = name;
+    document.getElementById("userName").innerHTML = "Mayor " + name;
     document.getElementById("userCity").innerHTML = cityName;
     document.getElementById("waterLevel").innerHTML = waterLevel;
     document.getElementById("map").style.filter = "blur(0px)";
