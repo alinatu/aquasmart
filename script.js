df7040c801ed51456721a091823ab8afea41c4fe
Tomas Quat <tquatito@gmail.com>
2018-04-27 18:28:25 -0700

    Wrote function to handle the new game feature. Resets to the original loading page

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var $name;
 var $cityName;
 var $waterLevel = "<img src='images/waterdrop.png' alt='water'>";
+//function for the Play arrow button
 function myFunction() {
     $name = document.getElementById("name").value;
     $cityName = document.getElementById("cityName").value;
@@ -16,6 +17,7 @@ function myFunction() {
         document.getElementById("login").style.display = "none";
     }
 }
+//Function for the About button
 $(document).ready(function(){
   $("#aboutdiv").click(function(){
     document.getElementById("about").style.display = "block";
@@ -23,19 +25,29 @@ $(document).ready(function(){
     document.getElementById("gamediv").style.display = "none";
   });
 });
-
+//Function for the new game button
+$(document).ready(function(){
+    $("#newGameDiv").click(function(){
+        document.getElementById("about").style.display = "none";
+        document.getElementById("gamediv").style.display = "block";
+        document.getElementById("map").style.filter = "blur(3px)";
+        document.getElementById("login").style.display = "block";
+        document.getElementById("userCity").innerHTML = "";
+        document.getElementById("userName").innerHTML = "";
+        document.getElementById("waterLevel").innerHTML = "";
+        document.getElementById("setNames").reset();
+    });
+});
+//series of functions for checking the names entered are valid
 function isString(s) {
     return Object.prototype.toString.call(s) === "[object String]";
 }
-
 function hasNum(s) {
     return /\d/.test(s);
 }
-
 function validCityLength(s) {
     return s.length > 0 && s.length <= 20;
 }
-
 function validNameLength(s) {
     return s.length > 0 && s.length <= 20;
 }
