582bf5e044995623f6fff8728d6503219798ba3e
Kurtis Lawson <kurtislawson@live.com>
2018-05-01 11:35:31 -0700

    Edited waterdrops in javascript. Changed map image

--- scriptWorking.js
+++ scriptWorking.js
@@ -72,11 +72,8 @@ function validNameLength(s) {
         district1 = new component(439, 267, "images/District1.png", 250, 235, "image");
         district2 = new component(434, 311, "images/District2.png", 590, 450, "image");
         district3 = new component(468, 369, "images/District3.png", 1300, 300, "image");
-        drop1 = new droplet(50, 50, "images/waterdrop.png", 250, 235, true);
-        drop2 = new droplet(50, 50, "images/waterdrop.png", 590, 450, false);
-        drop3 = new droplet(50, 50, "images/waterdrop.png", 1300, 300, true);
       }
-
+      
       var myGameArea = {
         canvas : document.createElement("canvas"),
         start : function() {
@@ -85,27 +82,13 @@ function validNameLength(s) {
           this.context = this.canvas.getContext("2d");
           this.canvas.setAttribute("id", "map");
           document.getElementById("gamediv").appendChild(this.canvas);
-          this.interval = setInterval(updateGameArea, 1000);
-          
+          this.interval = setInterval(updateGameArea, 100);
         },
         clear : function() {
           this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
         }
       }
       
-      var option = {
-        name: "Option Name",
-        desc : "Option Description",
-        difficulty : 10,
-        waterSaved : 12000,
-        create : function(name, desc, difficulty, waterSaved) {
-          this.name = name;
-          this.desc = desc;
-          this.difficulty = difficulty;
-          this.waterSaved = waterSaved;
-        }
-      }
-
       function component(width, height, color, x, y, type) {
         this.type = type;
         if (type == "image") {
@@ -128,65 +111,17 @@ function validNameLength(s) {
             ctx.fillStyle = color;
             ctx.fillRect(this.x, this.y, this.width, this.height);
           }
-        } 
-      }
-
-      function droplet(width, height, image, x, y, show) {
-        this.image = new Image();
-        this.image.src = image;
+        }
         
-        this.width = width;
-        this.height = height;
-        this.x = x;
-        this.y = y;
-        
-        this.show = show;
-        
-        this.update = function() {
-          ctx = myGameArea.context;
-          var x = Math.floor((Math.random() * 2) + 1);
-          console.log(x);
-        
-          if(x == 1){
-            this.show = true;
-            console.log(show);
-          } else {
-            
-            this.show = false;
-            console.log(show);
-          }
-        
-          
-          if (show == true) {
-              ctx.drawImage(this.image, 
-                this.x, 
-                this.y, 
-                this.width, this.height);
-          } else {
-              this.x = -50;
-              this.y = -50;
-              ctx.drawImage(this.image,
-                this.x,
-                this.y,
-                this.width, this.height);
-          }
-        } 
       }
       
-
       function updateGameArea() {
         myGameArea.clear();
         gameMap.update();
         district1.update();
         district2.update();
         district3.update();
-        drop1.update();
-        drop2.update();
-        drop3.update();
       }
-      
-
-      
 /*
 function hasSpecial(s) {
     return /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(s);
