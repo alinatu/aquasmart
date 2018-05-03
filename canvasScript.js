c11eaf0687e0781e0291675b26e9b5841264c389
Kurtis Lawson <kurtislawson@live.com>
2018-05-03 12:58:50 -0700

    Percentage tracking almost working

--- canvasScript.js
+++ canvasScript.js
@@ -3,33 +3,33 @@
       function startGame() {
         myGameArea.start();
         
-        gameMap = new component(375, 400, "images/Map.png", 0, 0, "image");
-        district1 = new component(100, 100, "images/District1Redone.png", 65, 100, "image");
-        paint1 = new toggleComponent(100, 100, "images/paintstroke.png", 65, 100, false);
+        gameMap = new component(1920, 1080, "images/Map.png", 0, 0, "image");
+        district1 = new component(439, 267, "images/District1Redone.png", 300, 230, "image");
+        paint1 = new toggleComponent(439, 267, "images/paintstroke.png", 300, 230, false);
         
-        district2 = new component(100, 100, "images/District2.png", 125, 160, "image");
-        paint2 = new toggleComponent(100, 100, "images/paintstroke.png", 125, 160, false);
+        district2 = new component(434, 311, "images/District2.png", 600, 450, "image");
+        paint2 = new toggleComponent(434, 311, "images/paintstroke.png", 600, 450, false);
         
-        district3 = new component(100, 100, "images/District3.png", 250, 125, "image");
-        paint3 = new toggleComponent(100, 100, "images/paintstroke.png", 250, 125, false);
+        district3 = new component(468, 369, "images/District3.png", 1250, 350, "image");
+        paint3 = new toggleComponent(468, 369, "images/paintstroke.png", 1250, 350, false);
         
         drop1 = new toggleComponent(50, 50, "images/waterdrop.png", 250, 235, false);
         drop2 = new toggleComponent(50, 50, "images/waterdrop.png", 590, 450, false);
         drop3 = new toggleComponent(50, 50, "images/waterdrop.png", 1300, 300, false);
       }
-      
+
       var myGameArea = {
         canvas : document.createElement("canvas"),
         start : function() {
-          this.canvas.width = 375;
-          this.canvas.height = 400;
+          this.canvas.width = 1920;
+          this.canvas.height = 1080;
           this.context = this.canvas.getContext("2d");
           this.canvas.setAttribute("id", "map");
           this.canvas.addEventListener('mousedown', onDown, false);
           this.canvas.addEventListener('mousemove', mouseHover, false);
           this.canvas.addEventListener('click', mouseClick, false);
           document.getElementById("gamediv").appendChild(this.canvas);
-          this.interval = setInterval(updateGameArea, 1000);
+          this.interval = setInterval(updateGameArea, 20);
           
         },
         clear : function() {
@@ -37,6 +37,11 @@
         }
       }
       
+      var percent = {
+        x : 0,
+        y : 0
+      };
+
       function getMousePos(canvas, event) {
         var bounds = canvas.getBoundingClientRect();
         return {
@@ -50,24 +55,26 @@
         cy = event.pageY;
       }
       
-      function mouseHover() {
-          var mousePos = getMousePos(myGameArea.canvas, event);
-          var message = 'Current pos: ' + mousePos.x + ', ' + mousePos.y;
-          console.log(mousePos.x + ", " + mousePos.y);
-          if (mousePos.x > district1.x && mousePos.x < (district1.x + district1.width)
-              && mousePos.y > district1.y && mousePos.y < district1.y + district1.height) {
+      function mouseHover(event) {
+          var x = Math.floor(event.x / myGameArea.canvas.offsetWidth * 100);
+          var y = Math.floor(event.y/ myGameArea.canvas.offsetHeight * 100) - 74;
+        
+          var message = 'Current pos: ' + x + ', ' + y;
+          console.log(x + ", " + y);
+          if (x > 15 && x < 34
+              && y > 20 && y < 41) {
             paint1.show = true;
           } else {
             paint1.show = false;
           }
-          if (mousePos.x > district2.x && mousePos.x < (district2.x + district2.width)
-              && mousePos.y > district2.y && mousePos.y < district2.y + district2.height) {
+          if (x > 32 && x < 50
+              && y > 38 && y < 62) {
             paint2.show = true;
           } else {
             paint2.show = false;
           }
-          if (mousePos.x > district3.x && mousePos.x < (district3.x + district3.width)
-              && mousePos.y > district3.y && mousePos.y < district3.y + district3.height) {
+          if (x > 66 && x < 90
+              && y > 21 && y < 46) {
             paint3.show = true;
           } else {
             paint3.show = false;
@@ -76,7 +83,6 @@
       function mouseClick() {
         var mousePos = getMousePos(myGameArea.canvas, event);
         var message = 'Current pos: ' + mousePos.x + ', ' + mousePos.y;
-        console.log(mousePos.x + ", " + mousePos.y);
         if (mousePos.x > district1.x && mousePos.x < (district1.x + district1.width)
             && mousePos.y > district1.y && mousePos.y < district1.y + district1.height) {
             document.getElementById("option").style.display = "block";
