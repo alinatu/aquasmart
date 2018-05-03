cdeff7c19ceb91578e85c41a9c1165080da38e6b
Tomas Quat <tquatito@gmail.com>
2018-05-03 09:10:27 -0700

    re-created getGlobalScores.php which I accidentally deleted

---
+++ DB/getGlobalScores.php
@@ -0,0 +1,53 @@
+<?php
+    $methodType = $_SERVER['REQUEST_METHOD'];
+ 
+     $servername = "localhost";
+     $dblogin = "root";
+     $password = "";
+     $dbname = "aquasmart";
+ 
+     $data = array("status" => "fail", "msg" => "on $methodType");
+ 
+     if ($methodType === 'GET') {
+         if (isset($_GET['output'])) {
+             $output = $_GET['output'];
+ 
+             try {
+                 $conn =  new PDO("mysql:host=$servername;dbname=$dbname", $dblogin, $password);
+ 
+                 $conn >setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ 
+                 $sqlPlayer = "SELECT * FROM player ORDER BY user_score DESC";
+ 
+                 $statementPlayer = $conn->prepare($sqlPlayer);
+                 $statementPlayer->execute();
+                 $count = $statementPlayer->rowCount();
+                 /*
+                 $sqlScore = "SELECT * FROM scores ORDER BY user_score DESC";
+                 $statementScore = $conn >prepare($sqlScore);
+                 $statementScore >execute();
+                 $count2 = $statementScore >rowCount();
+                 */
+                 $data = array("status" => "success", "players" => $statementPlayer->fetchAll(PDO::FETCH_ASSOC));//, "scores" => $statementScore >fetchAll(PDO::FETCH_ASSOC));
+             } catch (PDOEXception $e) {
+                 $data = array("error", $e->getMessage());
+             }
+ 
+                 switch($output) {
+                     case "json":
+                         $data['status'] = 'success';
+                         $data['msg'] = 'Retrieving data as JSON';
+ 
+                     $json = json_encode($data);
+ 
+                     echo $json;
+                     break;
+                 }
+     } else {
+         echo "Need a type of output";
+     }
+ 
+ } else {
+     echo $data;
+ }
+?>
