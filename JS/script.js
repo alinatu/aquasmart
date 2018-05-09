d66d837ee0700ae8294bf95cb9a49d03944fd32b
alinatu <alinatu1@gmail.com>
2018-05-09 12:22:40 -0700

    fixed changing height of the option div

--- JS/script.js
+++ JS/script.js
@@ -95,7 +95,7 @@ $(document).ready(function(){
   //function for the back arrow from the option card
   $("#optionExit").click(function(){
     $("#option").css("display", "none");
-    $("#option").css("height", "350px");
+    $("#option").css("height", "330px");
     $("#decision").html("<p id='decisionDescription'></p><button id='option1' class='optionButton'></button><button id='more1' class='moreButton'>...</button><br><p id='description1'></p><button id='option2' class='optionButton'></button><button id='more2' class='moreButton'>...</button><br><p id='description2'></p><button id='option3' class='optionButton'></button><button id='more3' class='moreButton'>...</button><br><p id='description3'></p><p id='success'></p>");
     updateScore();
   });
